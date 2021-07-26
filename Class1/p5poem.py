b25eb8330793220df37545a9e12f815e077960e9
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-07-26 11:26:16 -0400

    Create p5poem.py

---
+++ Class1/p5poem.py
@@ -0,0 +1,8 @@
+word1=input("Enter a word")
+word2=input("Enter second word")
+word3=input("Enter third word")
+word4=input("Enter fourth word")
+print("Roses are", word1)
+print("Violets are", word2)
+print("Sugar is", word3)
+print("So are", word4)
