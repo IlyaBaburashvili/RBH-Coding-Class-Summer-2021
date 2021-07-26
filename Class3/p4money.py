9f1746a1368de179cd2f4f6d0b6b74b8bd6c7dda
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-07-26 11:55:06 -0400

    Create p4money.py

---
+++ Class3/p4money.py
@@ -0,0 +1,7 @@
+price=int(input("How much does the item cost?"))
+money=int(input("How much money do you have?"))
+if money>=price":
+    print("You can buy it")
+else:
+    print("You don't have enough")
+    print("You need", price-money, "dollars more")
