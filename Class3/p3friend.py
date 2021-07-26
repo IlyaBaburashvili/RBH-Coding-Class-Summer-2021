e79b945d02947b49d4735acbb5dfbb6e77923469
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-07-26 11:52:21 -0400

    Create p3friend.py

---
+++ Class3/p3friend.py
@@ -0,0 +1,8 @@
+myage=int(input("Enter your age"))
+friendage=int(input("Enter your friend's age"))
+if(myage>friendage):
+    print("You are older")
+elif(myage<friendage):
+    print("Your frind is older:)
+else:
+    print("You are the same age")
