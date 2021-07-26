3e3de048e9c74124973f4896e6d21d65e28d7780
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-07-26 11:47:03 -0400

    Create p1number.py

---
+++ Class3/p1number.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number"))
+if a<100:
+    print("The number is less than 100")
+elif a==100:
+    print("The number is equal to 100")
+else:
+    print("The number is bigger than 100")
+
