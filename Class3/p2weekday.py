8ab25feaa42fb17827127faaa591bd2480ecccdb
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-07-26 11:48:42 -0400

    Create p2weekday.py

---
+++ Class3/p2weekday.py
@@ -0,0 +1,17 @@
+day=input("What day is it today?")
+if day=="Monday":    
+    print("Weekday")
+elif day=="Tuesday":    
+    print("Weekday")
+elif day=="Wednesday":    
+    print("Weekday")
+elif day=="Thursday":    
+    print("Weekday")
+elif day=="Friday":    
+    print("Weekday")
+elif day=="Saturday":    
+    print("Weekdend")
+elif day=="Sunday":    
+    print("Weekdend")
+else:    
+    print("This is not a correct input")
