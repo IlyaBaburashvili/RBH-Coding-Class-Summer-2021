84eb85df76444d41e9688efe44e2c2684a914eb2
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-07-29 08:58:47 -0400

    Delete age.py

--- Class3/age.py
+++
@@ -1,9 +0,0 @@
-age=int(input("How old are you?"))
-if age<13:
-    print("You are a preteen")
-elif age>=13 and age<=19:
-    print("You are a teenager")
-elif age>=20 and age<=120:
-    print("You are an adult")
-else:
-    print("This is not a valid age")
