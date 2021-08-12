baad3078ed74ff7d201a74aed7b0dad14289aa30
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-08-12 13:41:03 -0400

    Create game2.py

---
+++ game2.py
@@ -0,0 +1,56 @@
+
+import random
+
+userscore=0
+computerscore=0
+for i in range(5):
+    user_choice=input("Enter r for Rock, p for Paper, or s for Scissor")
+    if user_choice=="r":
+        user_input="Rock"
+    elif user_choice=="p":
+        user_input="Paper"
+    elif user_choice=="s":
+        user_input="Scissor"
+    else:
+        print("This is not valid input")
+    number=random.randint(1,3)
+    if number==1:
+        computerchoice="Rock"
+    elif number==2:
+        computerchoice="Paper"
+    elif number==3:
+        computerchoice="Scissor"
+    print("You chose", user_input)
+    print("The computer chose", computerchoice)
+    if user_input=="Rock" and computerchoice=="Rock":
+        print("Tie")
+    elif user_input=="Rock" and computerchoice=="Paper":
+        print("You lose")
+        computerscore+=1
+    elif user_input=="Rock" and computerchoice=="Scissor":
+        print("You win")
+        userscore+=1
+    elif user_input=="Paper" and computerchoice=="Rock":
+        print("You win")
+        userscore+=1
+    elif user_input=="Paper" and computerchoice=="Paper":
+        print("Tie")
+    elif user_input=="Paper" and computerchoice=="Scissor":
+        print("You lose")
+        computerscore+=1
+    elif user_input=="Scissor" and computerchoice=="Rock":
+        print("You lose")
+        computerscore+=1
+    elif user_input=="Scissor" and computerchoice=="Paper":
+        print("You win")
+        userscore+=1
+    elif user_input=="Scissor" and computerchoice=="Scissor":
+        print("Tie")
+print("Your score is", userscore)
+print("The computer score is", computerscore)
+if userscore>computerscore:
+    print("You won")
+elif computerscore>userscore:
+    print("You lost")
+else:
+    print("Tie")
